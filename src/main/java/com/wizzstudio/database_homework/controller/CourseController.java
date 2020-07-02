0ffb3575adee7977dfa994e523e6948a9186158d
117503445 <t117503445@gmail.com>
2020-07-02 14:06:45 +0800

    增加 CourseGetDto
    0.4.11-SNAPSHOT

--- src/main/java/com/wizzstudio/database_homework/controller/CourseController.java
+++ src/main/java/com/wizzstudio/database_homework/controller/CourseController.java
@@ -1,7 +1,7 @@
 package com.wizzstudio.database_homework.controller;
 
+import com.wizzstudio.database_homework.dto.CourseGetDto;
 import com.wizzstudio.database_homework.dto.CourseSetDto;
-import com.wizzstudio.database_homework.entity.CourseEntity;
 import com.wizzstudio.database_homework.error.CustomException;
 import com.wizzstudio.database_homework.repository.CourseRepository;
 import org.springframework.data.domain.Page;
@@ -18,14 +18,14 @@ public class CourseController {
     }
 
     @GetMapping()
-    public Page<CourseEntity> pageQuery(@RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
+    public Page<CourseGetDto> pageQuery(@RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
                                         @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize
     ) {
-        return courseRepository.findAll(PageRequest.of(pageNum - 1, pageSize));
+        return courseRepository.findAll(PageRequest.of(pageNum - 1, pageSize)).map(CourseGetDto::fromEntity);
     }
 
     @PostMapping
-    public CourseEntity save(@RequestBody CourseSetDto courseSetDto) throws CustomException {
-        return courseRepository.save(courseSetDto.toEntity());
+    public CourseGetDto save(@RequestBody CourseSetDto courseSetDto) throws CustomException {
+        return CourseGetDto.fromEntity(courseRepository.save(courseSetDto.toEntity()));
     }
 }
---
+++ src/main/java/com/wizzstudio/database_homework/dto/CourseGetDto.java
@@ -0,0 +1,76 @@
+package com.wizzstudio.database_homework.dto;
+
+import com.wizzstudio.database_homework.entity.CourseEntity;
+import com.wizzstudio.database_homework.entity.ScoreEntity;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CourseGetDto {
+    private long courseId;
+    private String name;
+    private Integer type;//0 -> 必修  1 -> 限选  2 -> 任选
+    private double credit;//学分
+    private long teacherId;
+    private List<Long> scoresId;
+
+    public static CourseGetDto fromEntity(CourseEntity courseEntity) {
+        CourseGetDto courseGetDto = new CourseGetDto();
+        courseGetDto.setCourseId(courseEntity.getCourseId());
+        courseGetDto.setCredit(courseEntity.getCredit());
+        courseGetDto.setName(courseEntity.getName());
+        courseGetDto.setScoresId(courseEntity.getScoreEntities().stream().map(ScoreEntity::getScoreId).collect(Collectors.toList()));
+        courseGetDto.setTeacherId(courseEntity.getCourseId());
+        courseGetDto.setType(courseEntity.getType());
+        return courseGetDto;
+    }
+
+
+    public long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(long courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public void setCredit(double credit) {
+        this.credit = credit;
+    }
+
+    public long getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(long teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public List<Long> getScoresId() {
+        return scoresId;
+    }
+
+    public void setScoresId(List<Long> scoresId) {
+        this.scoresId = scoresId;
+    }
+}
