fda7dfae94d6552133b8796e03fcd3bac067a2ce
117503445 <t117503445@gmail.com>
2020-07-04 00:24:05 +0800

    增加约束 1个老师不能同时给1个班级上多门课程
    0.7.3-SNAPSHOT

--- src/main/java/com/wizzstudio/database_homework/controller/CourseController.java
+++ src/main/java/com/wizzstudio/database_homework/controller/CourseController.java
@@ -2,10 +2,12 @@ package com.wizzstudio.database_homework.controller;
 
 import com.wizzstudio.database_homework.dto.CourseGetDto;
 import com.wizzstudio.database_homework.dto.CourseSetDto;
+import com.wizzstudio.database_homework.entity.CourseEntity;
 import com.wizzstudio.database_homework.error.CustomException;
 import com.wizzstudio.database_homework.repository.CourseRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/course")
@@ -26,7 +28,11 @@ public class CourseController {
 
     @PostMapping
     public CourseGetDto save(@RequestBody CourseSetDto courseSetDto) throws CustomException {
-        return CourseGetDto.fromEntity(courseRepository.save(courseSetDto.toEntity()));
+        var courseEntity = courseSetDto.toEntity();
+        if (courseEntity.getTeacherEntity().getCourseEntities().stream().map(CourseEntity::getClassEntity).anyMatch(c -> c == courseEntity.getClassEntity())) {
+            throw new CustomException(HttpStatus.BAD_REQUEST, "1个老师不能同时给1个班级上多门课程");
+        }
+        return CourseGetDto.fromEntity(courseRepository.save(courseEntity));
     }
 
 
