1e64885e3d237f4f8d66ad5b8f3da833e613cff3
117503445 <t117503445@gmail.com>
2020-07-03 11:41:36 +0800

    修复 跨域 问题
    0.6.1-SNAPSHOT

--- src/main/java/com/wizzstudio/database_homework/config/CorsConfig.java
+++ src/main/java/com/wizzstudio/database_homework/config/CorsConfig.java
@@ -14,11 +14,12 @@ public class CorsConfig {
       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
       CorsConfiguration config = new CorsConfiguration();
       config.setAllowCredentials(true);
-      config.addAllowedOrigin("*"); // e.g. http://domain1.com
+      config.addAllowedOrigin("*");
       config.addAllowedHeader("*");
       config.addAllowedMethod("*");
+      config.addExposedHeader("*");
 
-      source.registerCorsConfiguration("/api/**", config);
+      source.registerCorsConfiguration("/**", config);
       return new CorsFilter(source);
    }
 
